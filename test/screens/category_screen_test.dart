b6217fbc5b22b008c9352b56a61f6680d4870b57
Willian M Vargas <willianm.vargas@gmail.com>
2023-12-01 10:52:18 -0300

    feat: implement category screen test

--- test/screens/category_screen_test.dart
+++ test/screens/category_screen_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meals/dummy/dummy_data.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/main.dart';
 
 void main() {
   testWidgets('category ...', (tester) async {
     await tester.pumpWidget(const MyApp());
 
-    DUMMY_CATEGORIES.map((e) => expect(find.text(e.title), findsOneWidget));
+    dummyCategories.map((e) => expect(find.text(e.title), findsOneWidget));
   });
 }
